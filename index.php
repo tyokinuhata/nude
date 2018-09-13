43ad5100ede60efc934826b57bf9c50b23a8300f
Kazuki Tobita <kazukiti201@gmail.com>
2018-09-14 00:43:53 +0900

    Fixed indent

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <title>Nude</title>
-</head>
-<body>
-<h1>Nude</h1>
-<form action="index.php" method="post">
-    Name: <input type="text" name="name"><br>
-    Comment: <input type="text" name="comment" size="60"><br>
-    <input type="submit" name="submit" value="Send">
-</form>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <title>Nude</title>
+    </head>
+    <body>
+        <h1>Nude</h1>
+        <form action="index.php" method="post">
+            Name: <input type="text" name="name"><br>
+            Comment: <input type="text" name="comment" size="60"><br>
+            <input type="submit" name="submit" value="Send">
+        </form>
+    </body>
 </html>
