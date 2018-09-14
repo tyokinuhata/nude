de8e606437c36bfee7abf25718e35e150ff404a5
Kazuki Tobita <kazukiti201@gmail.com>
2018-09-15 01:24:31 +0900

    Add Router

---
+++ core/Router.php
@@ -0,0 +1,47 @@
+<?php
+
+class Router
+{
+    protected $routes;
+
+    public function __construct($definitions)
+    {
+        $this->routes = $this->compile($definitions);
+    }
+
+    public function compile($definitions)
+    {
+        $routes = [];
+
+        foreach ($definitions as $url => $params) {
+            $tokens = explode('/', ltrim($url, '/'));
+
+            foreach ($tokens as $i => $token) {
+                if (strpos($token, ':') === 0) {
+                    $name = substr($token, 1);
+                    $token = '(?P<' . $name . '>[^/]+)';
+                }
+                $tokens[$i] = $token;
+            }
+
+            $pattern = '/' . implode('/', $tokens);
+            $routes[$pattern] = $params;
+        }
+
+        return $routes;
+    }
+
+    public function resolve($pathInfo)
+    {
+        if (substr($pathInfo, 0, 1) !== '/') $pathInfo = '/' . $pathInfo;
+
+        foreach ($this->routes as $pattern => $params) {
+            if (preg_match('#^' . $pattern . '$#', $pathInfo, $matches)) {
+                $params = array_merge($params, $matches);
+                return $params;
+            }
+        }
+
+        return false;
+    }
+}
