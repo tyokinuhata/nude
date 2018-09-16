c5c4f752ecb81d4fa2321a19355e25648ea4a685
Kazuki Tobita <kazukiti201@gmail.com>
2018-09-17 04:45:04 +0900

    Add DbRepository

--- core/DbRepository.php
+++ core/DbRepository.php
@@ -2,5 +2,33 @@
 
 class DbRepository
 {
+    protected $con;
 
+    public function __construct($con)
+    {
+        $this->setConnection($con);
+    }
+
+    public function setConnection($con)
+    {
+        $this->con = $con;
+    }
+
+    public function execute($sql, $params = [])
+    {
+        $stmt = $this->con->prepare($sql);
+        $stmt->execute($params);
+
+        return $stmt;
+    }
+
+    public function fetch($sql, $params = [])
+    {
+        return $this->execute($sql, $params)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function fetchAll($sql, $params = [])
+    {
+        return $this->execute($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
