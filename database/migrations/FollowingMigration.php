445ebfa8d96a5a20473df7630a04ea265682794a
Kazuki Tobita <kazukiti201@gmail.com>
2018-09-21 02:46:37 +0900

    Fix cmd & migrations

--- database/migrations/FollowingMigration.php
+++ database/migrations/FollowingMigration.php
@@ -8,11 +8,37 @@ class FollowingMigration extends Migration
 {
     public function up()
     {
-        // TODO: Implement up() method.
+        $sql = 'CREATE TABLE `following` (
+            `id` INTEGER PRIMARY KEY,
+            `user_id` INTEGER NOT NULL UNIQUE,
+            `following_id` INTEGER NOT NULL UNIQUE,
+            FOREIGN KEY (user_id) REFERENCES user(id),
+            FOREIGN KEY (following_id) REFERENCES user(id)
+        )';
+
+        try {
+            $this->databaseHandle->exec($sql);
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            echo $e->getTraceAsString();
+            die();
+        }
     }
 
     public function down()
     {
-        // TODO: Implement down() method.
+        try {
+            $file = new \SplFileObject($this->databaseSource, 'wb');
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            echo $e->getTraceAsString();
+            die();
+        }
+
+        $res = $file->fwrite('');
+        if ($res === false) {
+            echo "Couldn't save this file.";
+            die();
+        }
     }
 }
--- database/migrations/StatusMigration.php
+++ database/migrations/StatusMigration.php
@@ -8,7 +8,21 @@ class StatusMigration extends Migration
 {
     public function up()
     {
-        // TODO: Implement up() method.
+        $sql = 'CREATE TABLE `status` (
+            `id` INTEGER PRIMARY KEY,
+            `user_id` INTEGER NOT NULL,
+            `body` VARCHAR(255),
+            `created_at` DATETIME,
+            FOREIGN KEY (user_id) REFERENCES user(id)
+        )';
+
+        try {
+            $this->databaseHandle->exec($sql);
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            echo $e->getTraceAsString();
+            die();
+        }
     }
 
     public function down()
